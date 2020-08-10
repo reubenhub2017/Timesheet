3b5a4d15cfcf9b75d03e6c56b52f252b98d30c63
reubenhub2017 <reubenukah2017@gmail.com>
2020-08-10 15:39:57 -0400

    Hide sensitive information Finished

---
+++ db_connect.php
@@ -0,0 +1,20 @@
+<?php
+//we need the database info
+/*
+
+
+$host="localhost"; // Host name
+$username="#"; // Mysql username
+$password="#"; // Mysql password
+$db_name="#"; // Database name
+$tbl_name="#"; // Table name
+
+
+// Connect to server and select database.
+mysqli_connect("$host", "$username", "$password")or die("cannot connect");
+mysql_select_db("$db_name")or die("cannot select DB");
+*/
+ //the only that's has to change is the tbl...
+// if successfully insert data into database, displays message "Successful".
+
+?>
